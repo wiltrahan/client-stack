ee87684f81b22c4b31b73653739d46555662f12c
wiltrahan <twilorip@gmail.com>
2018-10-20 20:02:03 -0400

    update to ng 7.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { ClientsTableComponent } from './clients-table/clients-table.component';
 import { ClientInfoComponent } from './client-info/client-info.component';
 import { ClientFormComponent } from './client-info/client-form/client-form.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +20,8 @@ import { ClientFormComponent } from './client-info/client-form/client-form.compo
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
