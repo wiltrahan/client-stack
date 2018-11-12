0388994a6c7144c9ce1d1549ccae964bb68f4c20
wiltrahan <twilorip@gmail.com>
2018-11-12 15:35:21 -0500

    fill about pg

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -13,7 +13,8 @@ import {
   MatTableModule,
   MatPaginatorModule,
   MatDividerModule,
-  MatDialogModule
+  MatDialogModule,
+  MatExpansionModule
 } from '@angular/material';
 
 @NgModule({
@@ -31,7 +32,8 @@ import {
     MatTableModule,
     MatPaginatorModule,
     MatDividerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatExpansionModule
   ],
   exports: [
     MatButtonModule,
@@ -47,7 +49,8 @@ import {
     MatTableModule,
     MatPaginatorModule,
     MatDividerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
